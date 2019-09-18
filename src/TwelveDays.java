bd78ec2269343bf8e87b60ef5d37f2b8e08dcdc9
john a <Elect@10.0.0.238>
2019-09-17 18:27:07 -0700

    final lmao

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -13,7 +13,86 @@ public class TwelveDays {
 		doves();
 		partridge();
 		System.out.println("On the fourth day of chirstmas,\nmy true love gave to me");
+		birds();
+		hens();
+		doves();
+		partridge();
 		System.out.println("On the fifth day of chirstmas,\nmy true love gave to me");
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the sixth day of chirstmas,\nmy true love gave to me");
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the seventh day of chirstmas,\nmy true love gave to me");
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the eighth day of chirstmas,\nmy true love gave to me");
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the ninth day of chirstmas,\nmy true love gave to me");
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the tenth day of chirstmas,\nmy true love gave to me");
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the eleventh day of chirstmas,\nmy true love gave to me");
+		pipers();
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
+		System.out.println("On the twelfth day of chirstmas,\nmy true love gave to me");
+		drummers();
+		pipers();
+		lords();
+		ladies();
+		maids();
+		swans();
+		geese();
+		rings();
+		birds();
+		hens();
+		doves();
+		partridge();
 	}
 	public static void partridge() {
 		System.out.println("a partridge in a pear tree.\n ");
@@ -23,5 +102,32 @@ public class TwelveDays {
 	}
 	public static void hens() {
 		System.out.println("three french hens,");
+	}
+	public static void birds() {
+		System.out.println("four calling birds,");
+	}
+	public static void rings() {
+		System.out.println("five golden rings,");
+	}
+	public static void geese() {
+		System.out.println("six geese a-laying,");
+	}
+	public static void swans() {
+		System.out.println("seven swans a-swimming,");
+	}
+	public static void maids() {
+		System.out.println("eight maids a-milking,");
+	}
+	public static void ladies() {
+		System.out.println("nine ladies dancing,");
+	}
+	public static void lords() {
+		System.out.println("ten lords a-leaping,");
+	}
+	public static void pipers() {
+		System.out.println("eleven pipers piping,");
+	}
+	public static void drummers() {
+		System.out.println("Twelve drummers drumming,");
 	}
 }
